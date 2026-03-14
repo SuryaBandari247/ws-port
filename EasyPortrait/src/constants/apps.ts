31494a1baa0f0c7ac86a0d9809b9cb61b3917ac7
surya bandari <surya24.bandari@gmail.com>
2026-03-14 11:40:53 +0100

    SEO: add /passport-photo-maker redirect, embed tool CTA in all guides, update internal links

--- EasyPortrait/src/constants/apps.ts
+++ EasyPortrait/src/constants/apps.ts
@@ -6,7 +6,7 @@ export interface WithSwagApp {
 }
 
 export const WITHSWAG_APPS: WithSwagApp[] = [
-  { name: 'Passport Photo', path: '/portrait/', icon: 'Camera', description: 'Passport & visa photos for 50+ countries' },
+  { name: 'Passport Photo', path: '/passport-photo-maker/', icon: 'Camera', description: 'Passport & visa photos for 50+ countries' },
   { name: 'Photo Guides', path: '/guides/', icon: 'BookOpen', description: 'Country-specific photo requirements' },
   { name: 'Visa Tools', path: '/tools/', icon: 'Wrench', description: 'Free visa application tools' },
 ];
